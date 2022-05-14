40bfa37c82b7b5d0028656eccb6f76b3c96af23d
jeanzless <jeanzle.albalate208@gmail.com>
2022-05-14 12:00:32 +0100

    commit

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -4,47 +4,6 @@
 //   </h1>
 // }
 
-// export default App
-
-
-import React from "react"
-import Line from "./components/Line"
-
-function App() {
-  const [lines, setLines] = React.useState(undefined)
-
-  React.useEffect(() => {
-    async function fetchLines() {
-      // const resp = await fetch("https://api.tfl.gov.uk/line/mode/tube/status")
-      const resp = await fetch("http://hp-api.herokuapp.com/api/characters/")
-      const data = await resp.json()
-      setLines(data)
-      console.log(data)
-    }
-    fetchLines()
-
-    setInterval(() => {
-      fetchLines()
-    }, 300000)
-
-  }, [])
-
-
-  return <div className="container">
-    {lines ? lines.map(line => {
-      return <Line
-        key={line.id}
-        name={line.name}
-        // id={line.id}
-        // status={line.lineStatuses[0].statusSeverityDescription}
-      />
-    }) : <p>Awaiting lines...</p>}
-  </div>
-}
-
-
-export default App
-
 ///Week 11 01
 
 
